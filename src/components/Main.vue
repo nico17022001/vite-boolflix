c0a940f676d790d840d8b3171b5df962c7b293c3
nico17022001 <nico17022001boolean@gmail.com>
2023-05-07 18:14:25 +0200

    sistemato-layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -22,32 +22,45 @@ export default {
   <div id="main-wrapper">
       <!-- TITOLO SEZIONE -->
         <div class="section-title">
-        <h3>ORIGINALI NETFLIX</h3>
-      </div>
+          <h3>ORIGINALI NETFLIX</h3>
+        </div>
       <!-- CONTENITORE DEI TITOLI -->
-    <div id="cards-container">
-      <!-- CICLO I FILM -->
-      <Cards v-for="film in store.arrayFilm" 
-        :key="film.id"
-        :title="film.title"
-        :originalTitle="film.original_title"
-        :language="film.original_language"
-        :poster_path="film.poster_path"
-        :rate="Math.ceil(film.vote_average / 2)"
-      />
-    </div>
+      <div class="section">
+        <!-- TITOLO SEZIONE -->
+        <div class="section-title">
+          <h3>FILM</h3>
+        </div>
+        
+        <div id="cards-container">
+          <!-- CICLO I FILM -->
+          <Cards v-for="film in store.arrayFilm" 
+            :key="film.id"
+            :title="film.title"
+            :originalTitle="film.original_title"
+            :language="film.original_language"
+            :poster_path="film.poster_path"
+            :rate="Math.ceil(film.vote_average / 2)"
+          />
+        </div>
+      </div>
 
-    <!-- CICLO LE SERIE -->
-    <div id="cards-container">
-      <Cards v-for="serie in store.arraySerie" 
-        :key="serie.id"
-        :title="serie.name"
-        :originalTitle="serie.original_name"
-        :language="serie.original_language"
-        :poster_path="serie.poster_path"
-        :rate="Math.ceil(serie.vote_average / 2)"
-      />
-    </div>
+      <div class="secion"> 
+      <!-- TITOLO SEZIONE -->
+        <div class="section-title">
+          <h3>SERIE</h3>
+        </div>
+        <!-- CICLO LE SERIE -->
+        <div id="cards-container">
+          <Cards v-for="serie in store.arraySerie" 
+            :key="serie.id"
+            :title="serie.name"
+            :originalTitle="serie.original_name"
+            :language="serie.original_language"
+            :poster_path="serie.poster_path"
+            :rate="Math.ceil(serie.vote_average / 2)"
+          />
+        </div>
+      </div>
   </div>
 </template>
 
@@ -57,14 +70,17 @@ export default {
     height: 100%;
     padding: 30px 20px;
   }#cards-container{
-    background-color: rgba(255, 255, 255, 0.405);
     height: 100%;
     display: flex;
     flex-wrap: wrap;
   }
 
+  .section{
+    margin: 10px 0px;
+  }
   .section-title{
     color: white;
     font-size: large;
+    margin: 10px 5px;
   }
 </style>
--- src/components/partials/Cards.vue
+++ src/components/partials/Cards.vue
@@ -16,15 +16,14 @@ export default {
     <div class="card">
       <div class="img-card"><img :src=" `https://image.tmdb.org/t/p/w500${poster_path}`" alt=""></div>
       <div class="film-info">
-        <div class="title">Titotlo = {{ title }} </div>
-        <div class="original-title">Originale = {{ originalTitle }} </div>
-        <div class="rate"> Voto = {{ rate }}</div>
-        <div class="language">
-          Lingua:
+        <div class="title m-y-10">Titotlo = {{ title }} </div>
+        <div class="original-title m-y-10">Originale = {{ originalTitle }} </div>
+        <div class="rate" > {{ rate }}</div>
+        <div class="language m-y-10">
           <img :src="`/${language}.png`" :alt="language">
         </div>
 
-        <div class="star">
+        <div class="star m-y-10">
           <i class="fa-solid fa-star" style="color: #ffff00;" v-for="(star, i) in rate" :key="i"></i>
           <i class="fa-regular fa-star" v-for="(star, i) in 5 - rate" :key="i"></i>
         </div>
@@ -34,11 +33,48 @@ export default {
 
 <style lang="scss" scoped>
 .card{
-    background-color: aquamarine;
     width: calc(100% / 6);
-    height: 500px;
+    height: 371.6px;
+    position: relative;
+    color: white;
+    overflow: hidden;
+    padding: 2px;
+    cursor: pointer;
+  }.img-card{
+    height: 100%;
   }img{
-    width: 40px;
+    width: 100%;
+    height: 100%;
   }
 
+  .film-info{
+    position: absolute;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.366);
+    width: 100%;
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    top: 100%;
+    opacity: 0;
+  }.language img{
+    width: 30px;
+  }
+
+  .m-y-10{
+    margin: 10px 0px;
+  }
+
+  .rate{
+    display: none;
+  }
+
+  .card:hover {
+    .film-info{
+      transition: 1s;
+      opacity: 100;
+      top: 0px;
+    }
+  }
 </style>
